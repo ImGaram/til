14b99ced9f054be816d3ded4b2f2610e3c8aaa06
imgaram <s21049@gsm.hs.kr>
2021-11-03 08:06:25 +0900

    중간고사 연습

---
+++ Workspace/study/src/test/java/midtermexam/cross/Pra1.java
@@ -0,0 +1,21 @@
+package midtermexam.cross;
+
+class PlayingCard{
+	int kind;
+	int num;
+	static int width = 100;
+	static int height = 150;
+	
+	PlayingCard(int kind, int num) {
+		this.kind=kind;
+		this.num = num;
+	}
+}
+
+public class Pra1 {
+	public static void main(String[] args) {
+		PlayingCard pc = new PlayingCard(1, 5);
+		System.out.println(pc.kind +" "+pc.num);
+		System.out.println(pc.width+" "+pc.height);
+	}
+}
