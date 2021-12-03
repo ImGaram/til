34ea914b276004841fc4adc93c20168c08a2b65d
imgaram <s21049@gsm.hs.kr>
2021-12-03 15:13:59 +0900

    계수 정렬 수정

--- 자료구조/계수정렬.c
+++ 자료구조/계수정렬.c
@@ -2,15 +2,11 @@
 
 int main(void)
 {
-    int temp;
-    int count[6];
+    int count[6] = { 0 };
     int array[30] = { 1, 3, 2, 4, 3, 2, 5, 3, 1, 2,
                      3, 4, 4, 3, 5, 1, 2, 3, 5, 2,
                      3, 1, 4, 3, 5, 1, 2, 1, 1, 1 };
-    for (int i = 1; i <= 5; i++)
-    {
-        count[i] = 0;   // count 초기화
-    }
+    
     for (int i = 0; i < 30; i++)
     {
         count[array[i]]++;  // array의 중복되는 수 카운트
