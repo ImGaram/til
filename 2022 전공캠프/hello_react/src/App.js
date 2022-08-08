4f24cbbf4c032eacfab6eea69ed20bccdacabb09
imgaram <s21049@gsm.hs.kr>
2022-08-08 17:08:15 +0900

    validation 예제

--- 2022 전공캠프/hello_react/src/App.js
+++ 2022 전공캠프/hello_react/src/App.js
@@ -1,29 +1,7 @@
-// import logo from './logo.svg';
-import React, { Component } from 'react';
 import './App.css';
+import ValidationSample from './ValidationSample';
 
-// function App() {
-//   const name = '리액트'
-//   return (
-//     <div>
-//       <div className='react'>{name}</div>
-      
-//       <h1>들여쓰기가 이상한</h1>
-//       <h2>코드</h2>
-//       <p>입니다.</p>
-//     </div>
-//   );
-//   // <input/>
-// }
-
-class App extends Component {
-  render() {
-    const name = 'react';
-    return(
-      <div className='react'>
-        {name}
-      </div>
-    );
-  }
+const App= () => {
+  return <ValidationSample/>
 }
 export default App;
---
+++ 2022 전공캠프/hello_react/src/ValidationSample.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./ValidationSample.css"
+
+class ValidationSample extends Component {
+    state={
+        password:' ',
+        clicked:false,
+        validated: false
+    }
+
+    handleChange=(e)=> {
+        this.setState({
+            password:e.target.value
+        });
+    }
+
+    handleButton=()=> {
+        this.setState({
+            clicked:true,
+            validated:this.state.password==='000'
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <input type="text" value={this.state.password} onChange={this.handleChange} className={this.state.clicked ? (this.state.validated ? 'success' : 'failure'): ''}/>
+                <button onClick={this.handleButton}>검증하기</button>
+            </div>
+        )
+    }
+}
+
+export default ValidationSample
