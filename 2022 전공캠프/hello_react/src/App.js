482cd527f7d5a53519b8a6664b5225171cdc6d36
imgaram <s21049@gsm.hs.kr>
2022-08-08 13:31:19 +0900

    function 대신 class 사용

--- 2022 전공캠프/hello_react/src/App.js
+++ 2022 전공캠프/hello_react/src/App.js
@@ -1,18 +1,29 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  const name = '리액트'
-  return (
-    <div>
-      <div className='react'>{name}</div>
+// function App() {
+//   const name = '리액트'
+//   return (
+//     <div>
+//       <div className='react'>{name}</div>
       
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다.</p>
-    </div>
-  );
-  // <input/>
+//       <h1>들여쓰기가 이상한</h1>
+//       <h2>코드</h2>
+//       <p>입니다.</p>
+//     </div>
+//   );
+//   // <input/>
+// }
+
+class App extends Component {
+  render() {
+    const name = 'react';
+    return(
+      <div className='react'>
+        {name}
+      </div>
+    );
+  }
 }
 export default App;
