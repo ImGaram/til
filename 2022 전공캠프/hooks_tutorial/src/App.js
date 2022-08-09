643b5369627507a864880013b167767603f956dc
imgaram <s21049@gsm.hs.kr>
2022-08-09 13:43:46 +0900

    평균 구하기 예제

--- 2022 전공캠프/hooks_tutorial/src/App.js
+++ 2022 전공캠프/hooks_tutorial/src/App.js
@@ -2,18 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Info from './Info';
 import { useState } from 'react';
+import Average from './Average';
 
 const App =()=> {
-  const [visible, setVisible]=useState(false);
-  return(
-    <div>
-      <button onClick={()=>{
-        setVisible(!visible);
-      }}>{visible ? '보이기':'숨기기'}</button>
-      <hr/>
-      {visible && <Info/>}
-    </div>
-  );
+  return <Average/>
 }
 
 export default App;
