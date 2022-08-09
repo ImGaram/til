68d7e1ffc89f699ac6392a0111b640a3eacb6afa
imgaram <s21049@gsm.hs.kr>
2022-08-09 10:41:53 +0900

    랜덤색상 예제 08/09

--- 2022 전공캠프/life_cycle/src/App.js
+++ 2022 전공캠프/life_cycle/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import LifeCycleSample from './LifeCycleSample';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomColor() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+class App extends Component {
+  statr ={
+    color:'#000000'
+  }
+
+  handleClick=()=>{
+    this.setState({
+      color:getRandomColor()
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
+    );
+  }
 }
 
 export default App;
